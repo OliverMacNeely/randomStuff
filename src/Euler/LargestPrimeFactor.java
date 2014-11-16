de6c80308c852eb4866e8ff5c72ec904b6a1b262
OliverMacNeely <macneely.oliver.1@gmail.com>
2014-11-16 09:52:12 -0500

    Updated LargestPrimeFactor problem...whether it works or not.....

--- src/Euler/LargestPrimeFactor.java
+++ src/Euler/LargestPrimeFactor.java
@@ -5,10 +5,16 @@ package Euler;
  */
 public class LargestPrimeFactor {
     public static void main (String[] args) {
-        System.out.println(findGreatestPrimeFactor(600851475143));
+        System.out.println(findGreatestPrimeFactor(600851475143L));
     }
-    public int findGreatestPrimeFactor (Long n) {
-        Long number;
-        number = 600851475143;
+    public static Long findGreatestPrimeFactor(Long n) {
+        Long i;
+        for (i = (Long) (n - 1); i < n; i--) {
+            if (n % i == 0) {
+                break;
+            }
+        }
+
+        return i;
     }
 }
