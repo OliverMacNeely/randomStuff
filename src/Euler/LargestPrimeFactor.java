7b75d1e9d86a7841fcd946be2450eaab8a880028
OliverMacNeely <macneely.oliver.1@gmail.com>
2014-11-16 10:00:40 -0500

    Finished and bested Max, but creds to Max for help

--- src/Euler/LargestPrimeFactor.java
+++ src/Euler/LargestPrimeFactor.java
@@ -1,20 +1,14 @@
 package Euler;
 
-/**
- * Created by Oliver on 11/15/2014.
- */
 public class LargestPrimeFactor {
     public static void main (String[] args) {
         System.out.println(findGreatestPrimeFactor(600851475143L));
     }
-    public static Long findGreatestPrimeFactor(Long n) {
-        Long i;
-        for (i = (Long) (n - 1); i < n; i--) {
-            if (n % i == 0) {
-                break;
-            }
+    public static int findGreatestPrimeFactor(Long n) {
+        int i;
+        for (i = 2; i < n; i++) {
+            if (n % i == 0) n /= i;
         }
-
         return i;
     }
 }
