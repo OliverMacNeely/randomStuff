e824b8e2715a92edd119d0021728292e4b743082
OliverMacNeely <macneely.oliver.1@gmail.com>
2014-11-16 21:59:39 -0500

    Finished problem 4, works

--- src/Euler/LargestThreeDigitPalindrome.java
+++ src/Euler/LargestThreeDigitPalindrome.java
@@ -4,4 +4,22 @@ package Euler;
  * Created by Oliver on 11/16/2014.
  */
 public class LargestThreeDigitPalindrome {
+    public static void main(String [] args) {
+        String stringNum;
+        int startingNum = 0;
+        for (int i = 999; i > 900; i--){
+            for (int x = 999; x > 900; x--){
+                int num = x * i;
+                stringNum = "" + num;
+                String reverseNum = new StringBuffer(stringNum).reverse().toString();
+                if(stringNum.equals(reverseNum)){
+                    int newNum = Integer.parseInt(stringNum);
+                    if (newNum > startingNum){
+                        startingNum = newNum;
+                    }
+                }
+            }
+        }
+        System.out.print(startingNum);
+    }
 }
