f71278ac8322ed5925021e628b450c76f0d6fa96
OliverMacNeely <macneely.oliver.1@gmail.com>
2014-11-17 16:28:01 -0500

    After much table banging and swearing I finally finished the code and got the right answer. Yayyyyyy!

--- src/Euler/SmallestMultiple.java
+++ src/Euler/SmallestMultiple.java
@@ -4,5 +4,19 @@ package Euler;
  * Created by Oliver on 11/16/2014.
  */
 public class SmallestMultiple {
+    public static void main(String[] args) {
+        int smallestMultiple = 0;
+        int i;
+        for (i = 1; ; i++) {
+            int x;
+            for (x = 1; x <= 20; x++) {
+                if (i % x == 0) continue;
+                else break;
+            }
+            if (i % x == 0) break;
+        }
+        smallestMultiple = i;
+        System.out.println(smallestMultiple);
+    }
 
 }
