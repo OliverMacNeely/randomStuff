8da8ea1317bd2f6be04470fc6466eed06e149035
OliverMacNeely <macneely.oliver.1@gmail.com>
2014-11-18 15:26:59 -0500

    Code  for problem 7, works

---
+++ src/me/olivermac/Euler/BigPrime.java
@@ -0,0 +1,27 @@
+package me.olivermac.Euler;
+
+/**
+ * Created by Oliver on 11/18/2014.
+ */
+public class BigPrime {
+    public static void main(String[] args) {
+        int finalNum = 0, counter = 0, i;
+        for (i = 2;;i++) {
+            if (isPrime(i)) counter++; finalNum = i;
+            if (counter == 10001) break;
+        }
+        System.out.println(finalNum);
+    }
+
+    public static boolean isPrime(int num) {
+        for(int i = 2; i < num; i++){
+            if(num == 2) return true;
+
+            if(num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
