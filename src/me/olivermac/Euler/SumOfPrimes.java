ace6beeb6aa3f3a9e857be03b7c5cd30bf42adb4
OliverMacNeely <macneely.oliver.1@gmail.com>
2014-11-20 18:01:38 -0500

    Code for Problem 6, works

--- src/me/olivermac/Euler/SumOfPrimes.java
+++ src/me/olivermac/Euler/SumOfPrimes.java
@@ -7,7 +7,10 @@ public class SumOfPrimes {
     public static void main(String[] args) {
         long i, sum = 0;
         for (i = 2; i < 2000000;i++){
-          if (BigPrime.isPrime(i)) sum += i;
+          if (BigPrime.isPrime(i)){
+              System.out.println("Adding " + i + " to " + sum);
+              sum += i;
+          }
         }
         System.out.println(sum);
     }
