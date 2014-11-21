27ee67e289fcee30275c1662473afdff3b2e7319
OliverMacNeely <macneely.oliver.1@gmail.com>
2014-11-20 20:35:24 -0500

    Working on Problem 12, still some problems.

--- src/me/olivermac/Euler/HighlyDivisibleTriangularNumber.java
+++ src/me/olivermac/Euler/HighlyDivisibleTriangularNumber.java
@@ -1,10 +1,25 @@
 package me.olivermac.Euler;
 
+import me.olivermac.Euler.MasterClasses.Divisible;
+
 /**
  * Created by Oliver on 11/20/2014.
  */
 public class HighlyDivisibleTriangularNumber {
     public static void main(String[] args) {
-
+        int x;
+        int count = 0;
+        int i;
+        for (i = 1; ; i++){
+            for (x = 1; ;x++){
+                if (Divisible.isDivisible(outputTriangularNumbers(i), x)) {
+                    count++;
+                if(count == 500) System.out.println(outputTriangularNumbers(i));
+                }
+            }
+        }
+    }
+    static int outputTriangularNumbers(int num){
+        return ((num*(num +1))/2);
     }
 }
