5f200376b4d2bc245e5ec233c44c5950fe7d8396
OliverMacNeely <macneely.oliver.1@gmail.com>
2014-11-20 19:45:35 -0500

    Added MasterClasses and the function "isDivisible"

---
+++ src/me/olivermac/Euler/HighlyDivisibleTriangularNumber.java
@@ -0,0 +1,10 @@
+package me.olivermac.Euler;
+
+/**
+ * Created by Oliver on 11/20/2014.
+ */
+public class HighlyDivisibleTriangularNumber {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/me/olivermac/Euler/MasterClasses/Divisible.java
@@ -0,0 +1,10 @@
+package me.olivermac.Euler.MasterClasses;
+
+/**
+ * Created by Oliver on 11/20/2014.
+ */
+public class Divisible {
+    public static boolean isDivisible( int num, int factor){
+         return num%factor == 0;
+    }
+}
